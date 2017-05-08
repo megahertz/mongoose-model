65059b9e0b0413e01e7ee893d42ce5e3cb54839c
Alexey Prokhorov <megahertz@l96.ru>
2017-05-08 12:54:07 +0500

    Add Query alias

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { default as Model, Ref } from "./lib/Model";
 export { default as model } from "./lib/decorators/model";
 export { default as property } from "./lib/decorators/property";
+export { Query } from "mongoose";
