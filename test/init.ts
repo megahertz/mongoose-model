f02742e8b9c050172edacdf5dbf2eb5c086e752a
Alexey Prokhorov <megahertz@l96.ru>
2018-04-25 14:46:28 +0500

    Update to mongoose 5

--- test/init.ts
+++ test/init.ts
@@ -1,9 +1,8 @@
 import * as mongoose from "mongoose";
 
 before(() => {
-  (mongoose as any).Promise = global.Promise;
   return (mongoose as any).connect("mongodb://localhost/mongoose-model", {
-    useMongoClient: true,
+    promiseLibrary: global.Promise,
   });
 });
 
